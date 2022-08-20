2c08512cb5299164001284ab022b294da14c8d13
Joy Bhalla <joybhalla9@gmail.com>
2022-08-20 15:04:27 +0530

    added form functionality

--- taskmate/todolist/views.py
+++ taskmate/todolist/views.py
@@ -1,15 +1,24 @@
 #from http.client import HTTPResponse
 from asyncio import all_tasks
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from todolist.models import TaskList
+from todolist.forms import TaskForm
+
 # Create your views here.
 
 def todolist(request):
-    # get all the objects of this class i.e tasks and done status
-    all_tasks=TaskList.objects.all
+    # if post request, post user input to database
+    if request.method == "POST":
+        form=TaskForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+        return redirect('todolist')
+    else:
+        # get all the objects of this class i.e tasks and done status
+        all_tasks=TaskList.objects.all
 
-    return render(request,'todolist.html',{'all_tasks':all_tasks})
+        return render(request,'todolist.html',{'all_tasks':all_tasks})
 
 def contact(request):
     context={
