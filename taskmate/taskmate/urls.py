fcfaf344f9f99a15fcc237567bb085f9d4723f6b
Joy Bhalla <joybhalla9@gmail.com>
2022-08-23 16:30:21 +0530

    added about us page

--- taskmate/taskmate/urls.py
+++ taskmate/taskmate/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from todolist import views as todolist_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('task/',include('todolist.urls'))
+    path('task/',include('todolist.urls')),
+    path('contact', todolist_views.contact,name='contact'),
+    path('about', todolist_views.about,name='about'),
+    path('',todolist_views.index,name='index')
 ]
--- taskmate/todolist/views.py
+++ taskmate/todolist/views.py
@@ -78,3 +78,9 @@ def pending_task(request,task_id):
     task.save()
     
     return redirect('todolist')
+
+def index(request):
+    context={
+        'index_text':"Welcome Index page",
+    }
+    return render(request,'index.html',context)
