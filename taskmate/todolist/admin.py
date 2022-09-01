05c96322351dfe2cba2b31275ee03c9a11cfcd98
Joy Bhalla <joybhalla9@gmail.com>
2022-09-01 13:42:10 +0530

    removed everything

--- taskmate/todolist/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from todolist.models import TaskList
-# Register your models here.
-
-admin.site.register(TaskList)
--- taskmate/todolist/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from todolist.models import TaskList
-
-class TaskForm(forms.ModelForm):
-    class Meta:
-        model=TaskList
-        fields=['task','done']
